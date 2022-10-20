a8fa6c65d4d8a312387b64998cb5612d7750a88d
aoudija <amineoudija02@yahoo.com>
2022-10-20 21:02:38 +0100

    striteri1.1

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,13 +6,13 @@
 /*   By: aoudija <aoudija@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 19:13:12 by aoudija           #+#    #+#             */
-/*   Updated: 2022/10/20 19:39:17 by aoudija          ###   ########.fr       */
+/*   Updated: 2022/10/20 20:58:08 by aoudija          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int , char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	unsigned int	i;
 	char			*ss;
@@ -21,7 +21,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int , char))
 	ss = ft_strdup(s);
 	while (ss[i])
 	{
-		f(i , ss[i]);
+		f(i, ss[i]);
 		i++;
 	}
 	return (ss);
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aoudija <aoudija@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 19:42:48 by aoudija           #+#    #+#             */
-/*   Updated: 2022/10/20 17:23:16 by aoudija          ###   ########.fr       */
+/*   Updated: 2022/10/20 21:00:40 by aoudija          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,4 +44,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strdup(const char *s1);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+
 #endif
