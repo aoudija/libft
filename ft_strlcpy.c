ec50c27731a5cd49ea061e78e11c95f99c3f909c
aoudija <amineoudija02@yahoo.com>
2022-10-25 12:22:06 +0100

    strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,25 +6,24 @@
 /*   By: aoudija <aoudija@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 12:42:37 by aoudija           #+#    #+#             */
-/*   Updated: 2022/10/09 13:46:07 by aoudija          ###   ########.fr       */
+/*   Updated: 2022/10/25 10:55:44 by aoudija          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	char	*d;
-	char	*s;
 	size_t	i;
 
-	d = (char *) dst;
-	s = (char *) src;
 	i = 0;
-	while (i < dstsize)
+	if (dst == NULL || size == 0)
+		return (ft_strlen (src));
+	while (src[i] && i < size - 1)
 	{
-		d[i] = s[i];
+		dst[i] = src[i];
 		i++;
 	}
-	return (i);
+	dst[i] = 0;
+	return (ft_strlen(src));
 }
