62bcfac8d1f3acef36620d85e24e0d6c99e3e9a9
amineoudija02@yahoo.com <amineoudija02@yahoo.com>
2022-10-27 23:44:02 +0100

    27_oct_2.0

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,68 +6,43 @@
 /*   By: aoudija <aoudija@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 15:22:17 by aoudija           #+#    #+#             */
-/*   Updated: 2022/10/27 17:48:37 by aoudija          ###   ########.fr       */
+/*   Updated: 2022/10/27 23:42:01 by aoudija          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	index_start(char const *s1, char const *set)
+int	index_start(const char *s1, const char *set)
 {
-	int		j;
-	int		i;
+	size_t	i;
+	size_t	j;
 
 	i = 0;
-	j = ft_strlen(s1);
+	j = ft_strlen(s1);	
 	while (i < j)
 	{
-		if (ft_strchr(set, s1[i])== 0)
-			break;
+		if (ft_strchr(set, s1[i]) == 0)
+			break ;
 		i++;
 	}
 	return (i);
 }
 
-int	index_end(char const *s1, char const *set)
+int	index_end(const char *s1, const char *set)
 {
-	int		j;
-	int		i;
+	size_t	i;
+	size_t	j;
 
 	i = 0;
 	j = ft_strlen(s1);
 	while (i < j)
 	{
-		if (ft_strchr(set, s1[len - i - 1])== 0)
-			break;
+		if (ft_strchr(set, s1[j - i - 1]) == 0)
+			break ;
 		i++;
 	}
-	return (i);
+	return (j - i);
 }
-
-// char	*ft_strtrim(char const *s1, char const *set)
-// {
-// 	char	*tri;
-// 	int		i;
-// 	int		j;
-// 	int		x;
-
-// 	x = 0;
-// 	i = index_start(s1, set);
-// 	j = index_end(s1, set);
-// 	tri = (char *)malloc(j - i + 1);
-// 	if (tri == NULL)
-// 		return (NULL);
-// 	if (s1 == NULL)
-// 		return (NULL);
-// 	while (i <= j)
-// 	{
-// 		tri [x] = s1[i];
-// 		i++;
-// 		x++;
-// 	}
-// 	tri[x] = 0;
-// 	return (tri);
-// }
 
 char		*ft_strtrim(char const *s1, char const *set)
 {
@@ -75,17 +50,17 @@ char		*ft_strtrim(char const *s1, char const *set)
 	int		j;
 	char	*tr;
 
-	if (s1 == NULL)
-		return (NULL);
 	i = index_start(s1, set);
 	j = index_end(s1, set);
+	if (s1 == NULL)
+		return (NULL);
 	if (set == NULL)
 		return (ft_strdup(s1));
 	// if (i >= j)
 	// 	return (ft_strdup(""));
-	tr = (char *)malloc(sizeof(char) * (i - j + 1));
+	tr = (char *)malloc(sizeof(char) * (j - i + 1));
 	if (tr == NULL)
 		return (NULL);
 	ft_strlcpy(tr, s1 + i, j - i + 1);
-	return (newstr);
+	return (tr);
 }
