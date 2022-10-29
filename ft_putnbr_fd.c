719cc7b8cfc7fa37a5f2e06362119c0bbd18e780
aoudija <amineoudija02@yahoo.com>
2022-10-29 13:55:37 +0100

    lhamdullah_anyway

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: aoudija <aoudija@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 21:17:16 by aoudija           #+#    #+#             */
-/*   Updated: 2022/10/29 13:39:27 by aoudija          ###   ########.fr       */
+/*   Updated: 2022/10/29 13:52:13 by aoudija          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,22 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*s;
+	long nb = n;
+	if (nb < 0)
+	{
+		ft_putchar_fd('-',fd);
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr_fd(nb / 10,fd);
+		nb = nb % 10;
+	}
+	if (nb < 10)
+		ft_putchar_fd(nb + 48,fd);
+	// char	*s;
 
-	s = ft_itoa(n);
-	ft_putstr_fd(s, fd);
-	free(s);
+	// s = ft_itoa(n);
+	// ft_putstr_fd(s, fd);
+	// free(s);
 }
